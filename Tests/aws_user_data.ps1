5d8d39932610ea29249442f4551082db211e1f74
Matt Richardson <matt@redgumtech.com.au>
2016-11-29 13:01:12 +1100

    Fix typos in some comments

--- Tests/aws_user_data.ps1
+++ Tests/aws_user_data.ps1
@@ -3,7 +3,7 @@ Write-Output -ForegroundColor green "Bootstrapping machine"
 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
 
-Write-Output "Setting up winrm"
+Write-Output "Setting up WinRM"
 winrm quickconfig -q
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
@@ -14,10 +14,10 @@ Write-Output "Setting up firewall"
 netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
 netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
 
-Write-Output "Restarting winrm"
+Write-Output "Restarting WinRM"
 net stop winrm
 & c:\windows\system32\sc.exe config winrm start= auto
 net start winrm
 
-Write-Output "Cygwin configuration complete."
+Write-Output "WinRM configuration complete."
 </powershell>
