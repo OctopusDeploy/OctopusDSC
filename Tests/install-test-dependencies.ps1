0fa71dc6f9efb5cd31b0bc2c907bee2b24204be6
Matt Richardson <matt@redgumtech.com.au>
2017-03-15 16:52:42 +1100

    lock version of bundler to 1.14.4
    to fix issue:
    
    C:/tools/ruby23/lib/ruby/2.3.0/fiddle/pack.rb:7:in `<module:PackInfo>':
    uninitialized constant Fiddle::PackInfo::TYPE_VOIDP (NameError)

--- Tests/install-test-dependencies.ps1
+++ Tests/install-test-dependencies.ps1
@@ -2,7 +2,7 @@
 echo "##teamcity[blockOpened name='Installing gem bundle']"
 
 Set-Location c:\temp\tests
-& C:\tools\ruby23\bin\bundle.bat install --path=vendor
+& C:\tools\ruby23\bin\bundle.bat _1.14.4_ install --path=vendor
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 echo "##teamcity[blockClosed name='Install gem bundle']"
