06d999369ec614d4a4536788d88430a563393c83
Jan Lønsetteig <jan.lonsetteig@confirmit.com>
2017-10-11 11:49:21 +0200

    Added sample configuration for custom comms port

--- OctopusDSC/Examples/cTentacleAgent_ListeningTentacle.psm1
+++ OctopusDSC/Examples/cTentacleAgent_ListeningTentacle.psm1
@@ -33,3 +33,41 @@ Configuration SampleConfig
         }
     }
 }
+
+Configuration SampleConfigWithCustomCommsPort
+{
+    param ($ApiKey, $OctopusServerUrl, $Environments, $Roles, $TentacleCommsPort)
+
+    Import-DscResource -Module OctopusDSC
+
+    Node "localhost"
+    {
+        cTentacleAgent OctopusTentacle
+        {
+            Ensure = "Present"
+            State = "Started"
+
+            # Tentacle instance name. Leave it as 'Tentacle' unless you have more
+            # than one instance
+            Name = "Tentacle"
+
+            # Registration - all parameters required
+            ApiKey = $ApiKey
+            OctopusServerUrl = $OctopusServerUrl
+            Environments = $Environments
+            Roles = $Roles
+
+            # How Tentacle will communicate with the server
+            CommunicationMode = "Listen"
+            ListenPort = 10933
+            #The call back port is different from the listen port. e.g the tentacle is behind a loadbalancer
+            TentacleCommsPort = $TentacleCommsPort
+
+            # Where deployed applications will be installed by Octopus
+            DefaultApplicationDirectory = "C:\Applications"
+
+            # Where Octopus should store its working files, logs, packages etc
+            TentacleHomeDirectory = "C:\Octopus"
+        }
+    }
+}
