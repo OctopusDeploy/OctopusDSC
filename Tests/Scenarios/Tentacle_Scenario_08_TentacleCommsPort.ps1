71e4b7d70cf3cb42ad4572b4f991b4ab3e73f244
Matt Richardson <matt.richardson@octopus.com>
2017-10-23 10:38:10 +1100

    Add e2e tests for tentacle-comms-port

---
+++ Tests/Scenarios/Tentacle_Scenario_08_TentacleCommsPort.ps1
@@ -0,0 +1,37 @@
+Configuration Tentacle_Scenario_08_TentacleCommsPort
+{
+    param ($OctopusServerUrl, $ApiKey, $Environments, $Roles)
+
+    Import-DscResource -ModuleName OctopusDSC
+
+    Node "localhost"
+    {
+        LocalConfigurationManager
+        {
+            DebugMode = "ForceModuleImport"
+        }
+
+        cTentacleAgent OctopusTentacle
+        {
+            Ensure = "Present";
+            State = "Started";
+
+            # Tentacle instance name. Leave it as 'Tentacle' unless you have more
+            # than one instance
+            Name = "Tentacle";
+
+            # Registration - all parameters required
+            ApiKey = $ApiKey;
+            OctopusServerUrl = $OctopusServerUrl;
+            Environments = $Environments;
+            Roles = $Roles;
+
+            # Optional settings
+            ListenPort = 10933;
+            TentacleCommsPort = 10900;
+            DefaultApplicationDirectory = "C:\Applications"
+            PublicHostNameConfiguration = "ComputerName"
+            TentacleHomeDirectory = "C:\Octopus\OctopusTentacleHome"
+        }
+    }
+}
