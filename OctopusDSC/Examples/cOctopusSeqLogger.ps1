2c8809e18abfd21128be17597aea60f4a2e16f3e
Matt Richardson <matt.richardson@octopus.com>
2017-11-15 05:52:12 +1100

    New resource to pump octopus logs to seq

---
+++ OctopusDSC/Examples/cOctopusSeqLogger.ps1
@@ -0,0 +1,19 @@
+Configuration SampleConfig
+{
+    Import-DscResource -Module OctopusDSC
+
+    Node "localhost"
+    {
+        $pass = ConvertTo-SecureString "Password" -AsPlainText -Force
+        $apiKeyCreds = New-Object System.Management.Automation.PSCredential ("ignored", $pass)
+
+        cOctopusSeqLogger "Enable Logging to Seq for Octopus Server"
+        {
+            InstanceType = "OctopusServer"
+            Ensure = 'Present'
+            SeqServer = 'https://seq.example.com'
+            SeqApiKey = $apiKeyCreds
+            Properties = @{ Application = 'Octopus'; 'ApplicationSet': 'BuildAndDeploy' }
+        }
+    }
+}
