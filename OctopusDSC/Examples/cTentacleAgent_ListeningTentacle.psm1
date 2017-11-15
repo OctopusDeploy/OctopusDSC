58602a300f499596c991d8b5830330e073f33bba
stopthatastronaut <stopthatastronaut@gmail.com>
2017-11-15 16:36:35 +1100

    fixes #76, fixes #74

--- OctopusDSC/Examples/cTentacleAgent_ListeningTentacle.psm1
+++ OctopusDSC/Examples/cTentacleAgent_ListeningTentacle.psm1
@@ -1,3 +1,7 @@
+# deserialize a password from disk.
+$password = Get-Content .\ExamplePassword.txt | ConvertTo-SecureString
+$ServiceCred = New-Object PSCredential "ServiceUser", $password 
+
 Configuration SampleConfig
 {
     param ($ApiKey, $OctopusServerUrl, $Environments, $Roles, $ListenPort)
@@ -24,6 +28,8 @@ Configuration SampleConfig
             # How Tentacle will communicate with the server
             CommunicationMode = "Listen"
             ListenPort = $ListenPort
+
+            TentacleServiceCredential = $ServiceCred
 
             # Only required if the external port is different to the ListenPort. e.g the tentacle is behind a loadbalancer
             TentacleCommsPort = 10900
