05c06a1194bd41d3b9a18d2e0c25da69ae116801
Matt Richardson <matt.richardson@octopus.com>
2017-11-17 07:48:51 +1100

    Import missing module to fix warning

--- Tests/Scenarios/Server_Scenario_10_Install4.ps1
+++ Tests/Scenarios/Server_Scenario_10_Install4.ps1
@@ -9,6 +9,7 @@
 Configuration Server_Scenario_10_Install4
 {
     Import-DscResource -ModuleName OctopusDSC
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
 
     $pass = ConvertTo-SecureString "SuperS3cretPassw0rd!" -AsPlainText -Force
     $cred = New-Object System.Management.Automation.PSCredential ("OctoAdmin", $pass)
