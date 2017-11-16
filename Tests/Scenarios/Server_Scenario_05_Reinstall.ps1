b6f2c75fc14a117dc1ed81362a5a142d28ad468d
Matt Richardson <matt.richardson@octopus.com>
2017-11-16 16:15:29 +1100

    Create new api key when using new database

--- Tests/Scenarios/Server_Scenario_05_Reinstall.ps1
+++ Tests/Scenarios/Server_Scenario_05_Reinstall.ps1
@@ -48,5 +48,58 @@ Configuration Server_Scenario_05_Reinstall
             EnvironmentName = "UAT 1"
         }
 
+        Script "Create Api Key and set environment variables for tests"
+        {
+            SetScript = {
+                Add-Type -Path "${env:ProgramFiles}\Octopus Deploy\Octopus\Newtonsoft.Json.dll"
+                Add-Type -Path "${env:ProgramFiles}\Octopus Deploy\Octopus\Octopus.Client.dll"
+
+                #connect
+                $endpoint = new-object Octopus.Client.OctopusServerEndpoint "http://localhost:81"
+                $repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+                #sign in
+                $credentials = New-Object Octopus.Client.Model.LoginCommand
+                $credentials.Username = "OctoAdmin"
+                $credentials.Password = "SuperS3cretPassw0rd!"
+                $repository.Users.SignIn($credentials)
+
+                #create the api key
+                $user = $repository.Users.GetCurrent()
+                $createApiKeyResult = $repository.Users.CreateApiKey($user, "Octopus DSC Testing")
+
+                #save it to enviornment variables for tests to use
+                [environment]::SetEnvironmentVariable("OctopusServerUrl", "http://localhost:81", "User")
+                [environment]::SetEnvironmentVariable("OctopusServerUrl", "http://localhost:81", "Machine")
+                [environment]::SetEnvironmentVariable("OctopusApiKey", $createApiKeyResult.ApiKey, "User")
+                [environment]::SetEnvironmentVariable("OctopusApiKey", $createApiKeyResult.ApiKey, "Machine")
+            }
+            TestScript = {
+                Add-Type -Path "${env:ProgramFiles}\Octopus Deploy\Octopus\Newtonsoft.Json.dll"
+                Add-Type -Path "${env:ProgramFiles}\Octopus Deploy\Octopus\Octopus.Client.dll"
+
+                #connect
+                $endpoint = new-object Octopus.Client.OctopusServerEndpoint "http://localhost:81"
+                $repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+                #sign in
+                $credentials = New-Object Octopus.Client.Model.LoginCommand
+                $credentials.Username = "OctoAdmin"
+                $credentials.Password = "SuperS3cretPassw0rd!"
+                $repository.Users.SignIn($credentials)
+
+                #check if the api key exists
+                $user = $repository.Users.GetCurrent()
+                $apiKeys = $repository.Users.GetApiKeys($user)
+                $apiKey = $apiKeys | where-object {$_.Purpose -eq "Octopus DSC Testing"}
+
+                return $null -ne $apiKey
+            }
+            GetScript = {
+                @{
+                    Result = "" #probably bad
+                }
+            }
+        }
     }
 }
