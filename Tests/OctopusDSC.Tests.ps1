37698702c7c5417318ceff451c5a0ed06712ac37
Matt Richardson <matt.richardson@octopus.com>
2017-12-18 15:32:46 +1100

    Dont run PSScriptAnalyzer on mac, as it doesn't work so well
    See https://github.com/PowerShell/PowerShell/issues/5707

--- Tests/OctopusDSC.Tests.ps1
+++ Tests/OctopusDSC.Tests.ps1
@@ -6,9 +6,24 @@ Describe "PSScriptAnalyzer" {
         'PSUseShouldProcessForStateChangingFunctions'
     )
     $excludedRules | % { Write-Warning "Excluding Rule $_" }
-    $results = Invoke-ScriptAnalyzer ./OctopusDSC -recurse -exclude $excludedRules
+
+    Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/DSCResources"
+    $results = Invoke-ScriptAnalyzer ./OctopusDSC/DSCResources -recurse -exclude $excludedRules
     $results | Out-File PsScriptAnalyzer.log
-    Write-Host ($results | ConvertTo-Json)
+
+    Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/Tests"
+    $results += Invoke-ScriptAnalyzer ./OctopusDSC/Tests -recurse -exclude $excludedRules
+    $results | Out-File PsScriptAnalyzer.log -Append
+
+    # PsScriptAnalyzer tests dont seem to work so well on mac - https://github.com/PowerShell/PowerShell/issues/5707
+    if (($null -eq $IsMacOS) -or ($IsMacOS -eq $false)) {
+        Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/Examples"
+        $results += Invoke-ScriptAnalyzer ./OctopusDSC/Examples -recurse -exclude $excludedRules
+        $results | Out-File PsScriptAnalyzer.log -Append
+    } else {
+        Write-Output "Skipping running PsScriptAnalyzer on `./OctopusDSC/Examples` as it doesn't run well on macOS."
+    }
+    Write-Output ($results | ConvertTo-Json)
     It "Should have zero PSScriptAnalyzer issues" {
         $results.length | Should Be 0
     }
