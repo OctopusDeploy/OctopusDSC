b0997e6816f208915972225fb70e1c58c9650738
Matt Richardson <matt.richardson@octopus.com>
2017-12-18 16:21:18 +1100

    Copy DSC module into $env:psmodulepath so that it can be found by the tests

--- Tests/OctopusDSC.Tests.ps1
+++ Tests/OctopusDSC.Tests.ps1
@@ -17,9 +17,17 @@ Describe "PSScriptAnalyzer" {
 
     # PsScriptAnalyzer tests dont seem to work so well on mac - https://github.com/PowerShell/PowerShell/issues/5707
     if (($null -eq $IsMacOS) -or ($IsMacOS -eq $false)) {
+
+        $modulePath = Split-Path $PSCommandPath -Parent
+
+        Write-Output "Copying DSC module from '$modulePath' to '$($env:psmodulepath)'"
+        Copy-Item -Path $modulePath -destination $env:psmodulepath -Recurse
+
         Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/Examples"
         $results += Invoke-ScriptAnalyzer ./OctopusDSC/Examples -recurse -exclude $excludedRules
         $results | Out-File PsScriptAnalyzer.log -Append
+        Remove-Item -Force -Recurse "$(env:psmodulepath)\OctopusDSC"
+
     } else {
         Write-Output "Skipping running PsScriptAnalyzer on `./OctopusDSC/Examples` as it doesn't run well on macOS."
     }
