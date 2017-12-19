85401982284c55665286ecdb4ad779c2d750eeb7
Matt Richardson <matt.richardson@octopus.com>
2017-12-20 10:26:51 +1100

    Add support for setting user account for run-on-server scripts

--- OctopusDSC/Tests/cOctopusServer.Tests.ps1
+++ OctopusDSC/Tests/cOctopusServer.Tests.ps1
@@ -95,7 +95,6 @@ try
             }
 
             Context "Running HA" {
-
                 It 'Should call expected commands on octopus.server.exe' {
                     Mock Invoke-OctopusServerCommand
                     Mock Get-TargetResource { return @{ Ensure="Absent"; State="Stopped" } }
@@ -141,6 +140,100 @@ try
                     Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'license --console --instance HANode --free' }
                     Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'service --console --instance HANode --install --reconfigure --stop' }
                     Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'service --start --console --instance HANode' }
+                }
+            }
+
+            Context "Run-on-server user - new install" {
+                It 'Should call expected commands on octopus.server.exe' {
+                    Mock Invoke-OctopusServerCommand #{ param ($arguments) write-host $arguments}
+                    Mock Get-TargetResource { return @{ Ensure="Absent"; State="Stopped" } }
+                    Mock Get-RegistryValue { return "478389" } # checking .net 4.5
+                    Mock Install-MSI {}
+                    Mock Update-InstallState {}
+                    Mock Test-OctopusDeployServerResponding { return $true }
+                    Mock Test-OctopusVersionSupportsHomeDirectoryDuringCreateInstance { return $true }
+                    Mock Test-OctopusVersionRequiresDatabaseBeforeConfigure { return $true }
+                    Mock Test-OctopusVersionSupportsAutoLoginEnabled { return $true }
+                    Mock Test-OctopusVersionSupportsShowConfiguration { return $true }
+                    Mock Test-OctopusVersionNewerThan { return $true }
+                    Mock Test-OctopusVersionSupportsRunAsCredential { return $true }
+                    Mock ConvertFrom-SecureString { return "" } # mock this, as its not available on mac/linux
+
+                    $pass = ConvertTo-SecureString "S3cur3P4ssphraseHere!" -AsPlainText -Force
+                    $cred = New-Object System.Management.Automation.PSCredential ("Admin", $pass)
+
+                    $pass = ConvertTo-SecureString "S4cretPassword!" -AsPlainText -Force
+                    $runAsCred = New-Object System.Management.Automation.PSCredential ("runasuser", $pass)
+
+                    $haparams = @{
+                        Ensure = "Present";
+                        State = "Started";
+                        Name = "OctopusServer";
+                        WebListenPrefix = "http://localhost:82";
+                        SqlDbConnectionString = "Server=(local);Database=Octopus;Trusted_Connection=True;";
+                        OctopusAdminCredential = $cred;
+                        ListenPort = 10935;
+                        AllowCollectionOfAnonymousUsageStatistics = $false;
+                        HomeDirectory = "C:\Octopus";
+                        OctopusRunOnServerCredential = $runAsCred
+                    }
+
+                    Set-TargetResource @haparams
+
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 9 -Exactly
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'create-instance --console --instance OctopusServer --config \Octopus\OctopusServer-OctopusServer.config --home C:\Octopus' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq "database --instance OctopusServer --connectionstring $($haparams['SqlDbConnectionString']) --create --grant NT AUTHORITY\SYSTEM" }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq "configure --console --instance OctopusServer --upgradeCheck True --upgradeCheckWithStatistics False --webForceSSL False --webListenPrefixes $($haparams['WebListenPrefix']) --commsListenPort 10935 --autoLoginEnabled False" }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'service --console --instance OctopusServer --stop' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'admin --console --instance OctopusServer --username Admin --password S3cur3P4ssphraseHere!' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'license --console --instance OctopusServer --free' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'service --console --instance OctopusServer --install --reconfigure --stop' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'runonserver --instance OctopusServer --username runasuser --password S4cretPassword!' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'service --start --console --instance OctopusServer' }
+                }
+            }
+
+            Context "Run-on-server user - existing install" {
+                It 'Should call expected commands on octopus.server.exe' {
+                    Mock Invoke-OctopusServerCommand #{ param ($arguments) write-host $arguments}
+                    Mock Get-TargetResource { return @{ Ensure="Present"; State="Started"; DownloadUrl="https://octopus.com/downloads/latest/WindowsX64/OctopusServer" } }
+                    Mock Get-RegistryValue { return "478389" } # checking .net 4.5
+                    Mock Update-InstallState {}
+                    Mock Test-OctopusDeployServerResponding { return $true }
+                    Mock Test-OctopusVersionSupportsHomeDirectoryDuringCreateInstance { return $true }
+                    Mock Test-OctopusVersionRequiresDatabaseBeforeConfigure { return $true }
+                    Mock Test-OctopusVersionSupportsAutoLoginEnabled { return $true }
+                    Mock Test-OctopusVersionSupportsShowConfiguration { return $true }
+                    Mock Test-OctopusVersionNewerThan { return $true }
+                    Mock Test-OctopusVersionSupportsRunAsCredential { return $true }
+                    Mock ConvertFrom-SecureString { return "" } # mock this, as its not available on mac/linux
+
+                    $pass = ConvertTo-SecureString "S3cur3P4ssphraseHere!" -AsPlainText -Force
+                    $cred = New-Object System.Management.Automation.PSCredential ("Admin", $pass)
+
+                    $pass = ConvertTo-SecureString "S4cretPassword!" -AsPlainText -Force
+                    $runAsCred = New-Object System.Management.Automation.PSCredential ("runasuser", $pass)
+
+                    $haparams = @{
+                        Ensure = "Present";
+                        State = "Started";
+                        Name = "OctopusServer";
+                        WebListenPrefix = "http://localhost:82";
+                        SqlDbConnectionString = "Server=(local);Database=Octopus;Trusted_Connection=True;";
+                        OctopusAdminCredential = $cred;
+                        ListenPort = 10935;
+                        AllowCollectionOfAnonymousUsageStatistics = $false;
+                        HomeDirectory = "C:\Octopus";
+                        OctopusRunOnServerCredential = $runAsCred
+                    }
+
+                    Set-TargetResource @haparams
+
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 4 -Exactly
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq "configure --console --instance OctopusServer --upgradeCheck True --upgradeCheckWithStatistics False --webForceSSL False --webListenPrefixes $($haparams['WebListenPrefix']) --commsListenPort 10935 --home C:\Octopus --autoLoginEnabled False" }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'admin --console --instance OctopusServer --username Admin --password S3cur3P4ssphraseHere!' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'license --console --instance OctopusServer --free' }
+                    Assert-MockCalled -CommandName 'Invoke-OctopusServerCommand' -Times 1 -Exactly -ParameterFilter { ($arguments -join ' ') -eq 'runonserver --instance OctopusServer --username runasuser --password S4cretPassword!' }
                 }
             }
         }
--- Tests/OctopusDSC.Tests.ps1
+++ Tests/OctopusDSC.Tests.ps1
@@ -6,11 +6,11 @@ Describe "PSScriptAnalyzer" {
     $excludedRules | % { Write-Warning "Excluding Rule $_" }
 
     Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/DSCResources"
-    $results = Invoke-ScriptAnalyzer ./OctopusDSC/DSCResources -recurse -exclude $excludedRules
+    $results = @(Invoke-ScriptAnalyzer ./OctopusDSC/DSCResources -recurse -exclude $excludedRules)
     $results | Out-File PsScriptAnalyzer.log
 
     Write-Output "Running PsScriptAnalyzer against ./OctopusDSC/Tests"
-    $results += Invoke-ScriptAnalyzer ./OctopusDSC/Tests -recurse -exclude $excludedRules
+    $results += @(Invoke-ScriptAnalyzer ./OctopusDSC/Tests -recurse -exclude $excludedRules)
     $results | Out-File PsScriptAnalyzer.log -Append
 
     # it'd be nice to run the PsScriptAnalyzer on `./OctopusDSC/Examples`, but I couldn't get it to detect the DSCModule on mac nor on linux
