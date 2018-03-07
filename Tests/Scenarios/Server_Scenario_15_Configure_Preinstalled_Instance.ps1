61bb88437bdd05ae563cda97ee9446e8e4c2036d
Matt Richardson <matt.richardson@octopus.com>
2018-03-07 17:08:35 +1100

    Add test to go from installed to configured
    To test the process, but its also needed for the subsequent tentacle tests

---
+++ Tests/Scenarios/Server_Scenario_15_Configure_Preinstalled_Instance.ps1
@@ -0,0 +1,35 @@
+$pass = ConvertTo-SecureString "SuperS3cretPassw0rd!" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("OctoAdmin", $pass)
+
+Configuration Server_Scenario_15_Configure_Preinstalled_Instance
+{
+    Import-DscResource -ModuleName OctopusDSC
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+    Node "localhost"
+    {
+        LocalConfigurationManager
+        {
+            DebugMode = "ForceModuleImport"
+            ConfigurationMode = 'ApplyOnly'
+        }
+
+        cOctopusServer OctopusServer
+        {
+            Ensure = "Present"
+            State = "Started"
+            Name = "OctopusServer"
+            WebListenPrefix = "http://localhost:81"
+            SqlDbConnectionString = "Server=(local)\SQLEXPRESS;Database=OctopusScenario15;Trusted_Connection=True;"
+            OctopusAdminCredential = $cred
+            AllowCollectionOfUsageStatistics = $false
+        }
+
+        cOctopusServerUsernamePasswordAuthentication "Enable Username/Password Auth"
+        {
+            InstanceName = "OctopusServer"
+            Enabled = $true
+            DependsOn = "[cOctopusServer]OctopusServer"
+        }
+    }
+}
