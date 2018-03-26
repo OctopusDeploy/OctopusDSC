9a6de5c6ea3cf3ffc993d70872908e5a1788ebc3
Matt Richardson <matt.richardson@octopus.com>
2018-03-26 18:44:12 +1100

    Update tests after tweak of stop service call

--- OctopusDSC/Tests/OctopusServerExeInvocationFiles/NewInstallWithBuiltInWorker/ExpectedResult.ps1
+++ OctopusDSC/Tests/OctopusServerExeInvocationFiles/NewInstallWithBuiltInWorker/ExpectedResult.ps1
@@ -2,7 +2,7 @@ return @(
   "create-instance --console --instance OctopusServer --config $($env:SystemDrive)\Octopus\OctopusServer-OctopusServer.config --home C:\Octopus",
   "database --instance OctopusServer --connectionstring Server=(local);Database=Octopus;Trusted_Connection=True; --create --grant NT AUTHORITY\SYSTEM",
   "configure --console --instance OctopusServer --upgradeCheck True --upgradeCheckWithStatistics False --webForceSSL False --webListenPrefixes http://localhost:82 --commsListenPort 10935 --autoLoginEnabled False --hstsEnabled False --hstsMaxAge 3600",
-  "service --console --instance OctopusServer --stop",
+  "service --stop --console --instance OctopusServer",
   "admin --console --instance OctopusServer --username Admin --password S3cur3P4ssphraseHere!",
   "license --console --instance OctopusServer --free",
   "service --console --instance OctopusServer --install --reconfigure --stop",
