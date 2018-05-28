d705bc0181101a899ed4707d3be7c7db7dedb2f0
Jason Brown <stopthatastronaut@gmail.com>
2018-05-28 15:40:18 +1000

    The 'shared' regex is temperamental. throwing it away for now

--- OctopusDSC/OctopusDSCHelpers.ps1
+++ OctopusDSC/OctopusDSCHelpers.ps1
@@ -100,17 +100,15 @@ Function Get-MaskedOutput
     [CmdletBinding()]
     param($arguments)
 
-    $sensregex = [regex]"--masterkey|--password|--license"
-
     Write-Verbose "Masking output"
 
-    if(($arguments -match $sensregex) -gt 0)
+    if(($arguments -match "--masterkey|--password|--license") -gt 0)
     {
         Write-Verbose "We found a sensitive command line argument. Masking"
 
         for($x=0;$x -lt $arguments.count; $x++)
         {
-            if(($arguments[$x] -match $sensregex) -gt 0)
+            if(($arguments[$x] -match "--masterkey|--password|--license") -gt 0)
             {
                 $arguments[$x+1] = $arguments[$x+1] -replace ".", "*"
             }
