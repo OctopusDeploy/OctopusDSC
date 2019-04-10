29b99cb2b532d3d1f488878bf2676b8b84b20973
Matt Richardson <matt.richardson@octopus.com>
2019-04-10 18:48:51 +1000

    Allow a masterkey to be provided for a brand new db (#195)
    * Allow a masterkey to be provided for a brand new db
    
    Fixes https://github.com/OctopusDeploy/OctopusDSC/issues/194

--- OctopusDSC/Tests/OctopusServerExeInvocationFiles/MasterKeySupplied/ExpectedResult.ps1
+++ OctopusDSC/Tests/OctopusServerExeInvocationFiles/MasterKeySupplied/ExpectedResult.ps1
@@ -1,6 +1,6 @@
 return @(
   "create-instance --console --instance HANode --config $($env:SystemDrive)\Octopus\OctopusServer-HANode.config --home C:\ChezOctopusSecondNode",
-  "database --instance HANode --connectionstring Server=(local);Database=Octopus;Trusted_Connection=True; --masterKey Nc91+1kfZszMpe7DMne8wg== --grant NT AUTHORITY\SYSTEM",
+  "database --instance HANode --connectionstring Server=(local);Database=Octopus;Trusted_Connection=True; --create --masterKey Nc91+1kfZszMpe7DMne8wg== --grant NT AUTHORITY\SYSTEM",
   "configure --console --instance HANode --upgradeCheck True --upgradeCheckWithStatistics False --webForceSSL False --webListenPrefixes http://localhost:82 --commsListenPort 10935 --hstsEnabled False --hstsMaxAge 3600",
   "service --stop --console --instance HANode",
   "admin --console --instance HANode --username Admin --password S3cur3P4ssphraseHere!",
