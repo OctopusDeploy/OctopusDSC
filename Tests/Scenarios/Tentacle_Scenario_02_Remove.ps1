194b56854ee94fd8122b4f4881bccb5ff806fd6b
Matt Richardson <matt.richardson@octopus.com>
2019-10-22 09:47:11 +1100

    Quote role name

--- Tests/Scenarios/Tentacle_Scenario_02_Remove.ps1
+++ Tests/Scenarios/Tentacle_Scenario_02_Remove.ps1
@@ -2,7 +2,7 @@ $config = get-content "c:\temp\octopus-configured.marker" | ConvertFrom-Json
 $OctopusServerUrl = $config.OctopusServerUrl
 $ApiKey = $config.OctopusApiKey
 $Environments = "The-Env"
-$Roles = Test-Tentacle
+$Roles = "Test-Tentacle"
 $ServerThumbprint = $config.OctopusServerThumbprint
 
 Configuration Tentacle_Scenario_02_Remove
