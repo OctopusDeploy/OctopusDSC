8f5f0de64f7683b49e6e8ed09fa6157a86a1ef90
twerthi <twerthi@hotmail.com>
2019-10-25 15:43:12 -0700

    Newer versions of PowerShell allow for multiple versions of a module to be installed.  Added filter to select newest version in the Test-PowershellModuleInstalled function.  Moved import of module to Test-PowershellModuleInstalled.  If this breaks the function should only do one thing model, let me know and I'll make adjustments.

--- Tests/powershell-helpers.ps1
+++ Tests/powershell-helpers.ps1
@@ -38,17 +38,28 @@ function Test-AppExists($appName) {
 }
 
 function Test-PowershellModuleInstalled($moduleName, $version) {
+  Write-Output "Checking version for $moduleName..."
   $command = Get-Module $moduleName -listavailable
   if ($null -eq $command) {
     write-host "Please install $($moduleName): Install-Module -Name $moduleName -Force" -foregroundcolor red
     exit 1
   }
+
+  if ($command.Count -gt 1)
+  {
+    $command = $command | Sort-Object -Property Version -Descending | Select-Object -First 1
+  }
+
+
   if ($null -ne $version) {
     if ($command.Version -lt [System.Version]::Parse($version)) {
       write-host "Please install $($moduleName) $version or higher (you have version $($command.Version)): Update-Module -Name $moduleName -Force" -foregroundcolor red
       exit 1
     }
   }
+
+  Write-Output "Importing module $moduleName with version $($command.Version)..."
+  Import-Module -Name $moduleName -Version $command.Version
 }
 
 function Test-CustomVersionOfVagrantDscPluginIsInstalled() {  # does not deal well with machines that have two versioned folders under /gems/
