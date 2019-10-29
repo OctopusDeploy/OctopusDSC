94453efd099f64ee583d449b2f25cb076cafd609
Shawn Sesna <twerthi@hotmail.com>
2019-10-29 14:59:34 -0700

    Update build-aws.ps1
    Co-Authored-By: Matt Richardson <matt.richardson@octopus.com>

--- build-aws.ps1
+++ build-aws.ps1
@@ -45,7 +45,6 @@ if(-not $SkipPester) {
   Write-Output "Importing Pester module"
   Import-PowerShellModule -Name "Pester" -MinimumVersion "4.9.0"
   
-  
   Write-Output "Running Pester Tests"
   $result = Invoke-Pester -OutputFile PesterTestResults.xml -OutputFormat NUnitXml -PassThru
   if ($result.FailedCount -gt 0) {
