9906172203f97d574064ba5abee5992bc09e526f
Matt Richardson <matt.richardson@octopus.com>
2019-11-27 06:33:22 +1100

    Cleanup configure-octopus-for-tentacle-tests script

--- Tests/configure-octopus-for-tentacle-tests.ps1
+++ Tests/configure-octopus-for-tentacle-tests.ps1
@@ -28,53 +28,31 @@ try
     $createApiKeyResult = $repository.Users.CreateApiKey($user, "Octopus DSC Testing")
 
     #create an environment for the tentacles to go into
-    $environment = New-Object Octopus.Client.Model.EnvironmentResource
-    $environment.Name = "The-Env"
-    $repository.Environments.CreateOrModify($environment) | Out-Null
-    $environment = New-Object Octopus.Client.Model.EnvironmentResource
-    $environment.Name = "Env2"
-    $repository.Environments.CreateOrModify($environment) | Out-Null
+    $environment = $repository.Environments.CreateOrModify("The-Env") | Out-Null
+    $repository.Environments.CreateOrModify("Env2") | Out-Null
 
     #create a project
     $projectGroup = $repository.ProjectGroups.Get("ProjectGroups-1")
-    if($null -eq $projectGroup)
-    {
+    if($null -eq $projectGroup) {
       throw "Default Project group not found during configuration"
     }
 
     $lifecycle = $repository.Lifecycles.FindByName("Default Lifecycle")
-    if($null -eq $lifecycle)
-    {
+    if($null -eq $lifecycle) {
       throw "Lifecycle 'Default Lifecycle' not found during configuration"
     }
 
     $project = $repository.Projects.CreateOrModify("Multi tenant project", $projectGroup, $lifecycle)
-    $project.Save() | Out-Null
-
-    #enable Tenants feature  # as of 2019 versions, this is permanently on.
-    #$featureConfig = $repository.FeaturesConfiguration.GetFeaturesConfiguration()
-    #$featureConfig.IsMultiTenancyEnabled = $true
-    #$repository.FeaturesConfiguration.ModifyFeaturesConfiguration($featureConfig) | Out-Null
-
-    #reconnect after changing the feature config as the OctopusRepository caches some stuff
-    $endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURI
-    $repository = new-object Octopus.Client.OctopusRepository $endpoint
-    $repository.Users.SignIn($credentials)
 
     # setup tag set
     $tagSetEditor = $repository.TagSets.CreateOrModify("Hosting")
     $tagSetEditor.AddOrUpdateTag("On premises", "Hosted on site", [Octopus.Client.Model.TagResource+StandardColor]::DarkGreen) | Out-Null
     $tagSetEditor.AddOrUpdateTag("Cloud", "Hosted in the cloud", [Octopus.Client.Model.TagResource+StandardColor]::LightBlue) | Out-Null
-    $tagSetEditor.Save() | Out-Null
     $tagSet = $tagSetEditor.Instance
-
-    $project = $repository.Projects.FindByName("Multi tenant project")
-    $environment = $repository.Environments.FindByName("The-Env")
 
     $tenantEditor = $repository.Tenants.CreateOrModify("John")
     $tenantEditor.WithTag($tagSet.Tags[0]) | Out-Null
     $tenantEditor.ConnectToProjectAndEnvironments($project, $environment) | Out-Null
-    $tenantEditor.Save() | Out-Null
 
     # create a non-default Test Machine Policy w/ defaults
     $policyResource = $repository.MachinePolicies.GetTemplate()
