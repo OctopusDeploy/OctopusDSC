d175e1149fb354754a28c17685f0e766c7d7139e
Matt Richardson <matt.richardson@octopus.com>
2020-04-11 14:23:11 +1000

    Stop testing the cOctopusSeqLogger component temporarily
    As it's stopped working with 2020.1, as Octopus got converted to netcore, and can no longer load a dll that is dropped into the application directory

--- Tests/Spec/tentacle_scenario_01_install_spec.rb
+++ Tests/Spec/tentacle_scenario_01_install_spec.rb
@@ -239,21 +239,6 @@ describe file('C:\Octopus\ListeningTentacleWithCustomAccountHome\ListeningTentac
   its(:content) { should match /Tentacle\.Communication\.TrustedOctopusServers.*#{config['OctopusServerThumbprint']}/}
 end
 
-#seq logging
-describe file('C:/Program Files/Octopus Deploy/Tentacle/Seq.Client.NLog.dll') do
-  it { should be_file }
-end
-
-describe file('C:/Program Files/Octopus Deploy/Tentacle/Tentacle.exe.nlog') do
-  it { should be_file }
-  its(:content) { should match /<add assembly="Seq.Client.NLog" \/>/ }
-  its(:content) { should match /<logger name="\*" minlevel="Info" writeTo="seqbufferingwrapper" \/>/ }
-  its(:content) { should match /<target name=\"seqbufferingwrapper\" xsi:type=\"BufferingWrapper\" bufferSize=\"1000\" flushTimeout=\"2000\">/}
-  its(:content) { should match /<target name="seq" xsi:type="Seq" serverUrl="http:\/\/localhost\/seq" apiKey="MyMagicSeqApiKey">/ }
-  its(:content) { should match /<property name="Application" value="Octopus" \/>/ }
-  its(:content) { should match /<property name="Server" value="MyServer" \/>/ }
-end
-
 ### DSC success
 describe windows_dsc do
   it { should be_able_to_get_dsc_configuration }
