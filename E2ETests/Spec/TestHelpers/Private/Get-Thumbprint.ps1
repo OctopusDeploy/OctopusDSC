71591f70b9d9721a08bc9234f2984d2ec8ce00e5
Matt Richardson <matt.richardson@octopus.com>
2021-10-28 07:07:27 +1100

    Remove debug code

--- E2ETests/Spec/TestHelpers/Private/Get-Thumbprint.ps1
+++ E2ETests/Spec/TestHelpers/Private/Get-Thumbprint.ps1
@@ -7,8 +7,6 @@ function Get-Thumbprint {
         $InstanceName
     )
 
-    return "102C445D1F01C4C5957D5CFA53FF2D7CA80E84BB"
-
     $thumbprint = & "c:\program files\Octopus Deploy\Tentacle\Tentacle.exe" show-thumbprint --console --nologo --instance $InstanceName
     $thumbprint = $thumbprint -replace '==== ShowThumbprintCommand starting ====', ''
     $thumbprint = $thumbprint -replace 'The thumbprint of this Tentacle is: ', ''
