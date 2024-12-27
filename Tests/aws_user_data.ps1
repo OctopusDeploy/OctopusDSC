f634c4a6be20ef3b104c1a590549f688a89521a8
Matt Richardson <matt.richardson@octopus.com>
2024-12-27 15:24:43 +1100

    Update aws_user_data.ps1

--- Tests/aws_user_data.ps1
+++ Tests/aws_user_data.ps1
@@ -1,14 +1,18 @@
 <powershell>
-Write-Output -ForegroundColor green "Bootstrapping machine"
+Set-PSDebug -Trace 1
 
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
+Write-Output "Bootstrapping machine"
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
 
 Write-Output "Creating self signed cert"
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
-New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 
 Write-Output "Setting up WinRM"
 winrm quickconfig -q
+
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
+
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
 winrm set winrm/config/service '@{AllowUnencrypted="false"}'
@@ -17,6 +21,7 @@ winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hos
 
 Write-Output "Setting up firewall"
 netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+netsh advfirewall firewall show rule name="WinRM 5986"
 
 Write-Output "Restarting WinRM"
 net stop winrm
